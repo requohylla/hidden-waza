bebf649f43935c5bf113e0e8001cd68daf1792ca
requohylla <requohylla@gmail.com>
2025-07-29 23:25:28 +0900

    マイグレーションとseederの更新

--- golang/services/hidden_waza/db/seeder/dummydata/resume.go
+++ golang/services/hidden_waza/db/seeder/dummydata/resume.go
@@ -14,11 +14,14 @@ func GenerateResumes(n int) []domain.Resume {
 	resumes := make([]domain.Resume, n)
 	now := time.Now()
 	for i := 0; i < n; i++ {
+		verified := rand.Intn(2) == 0 // true/falseをランダム生成
+		fmt.Printf("resume[%d] verified: %v\n", i, verified)
 		resumes[i] = domain.Resume{
 			Title:     fmt.Sprintf("ダミーレジュメ%03d", i+1),
 			UserID:    uint(rand.Intn(100) + 1),
 			CreatedAt: now,
 			UpdatedAt: now,
+			Verified:  verified,
 		}
 	}
 	return resumes
