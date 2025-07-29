8690fae48552caf8ced3c0c7811ee79330bd338b
requohylla <requohylla@gmail.com>
2025-07-30 07:44:05 +0900

    実行権限付与と必要モジュールのインストール

--- golang/deploy/entrypoint.prod.sh
+++ golang/deploy/entrypoint.prod.sh
@@ -3,11 +3,11 @@ set -e
 
 # マイグレーション
 echo "=== Running goose migration ==="
-goose mysql "$DB_USER:$DB_PASSWORD@tcp($DB_HOST:$DB_PORT)/$DB_NAME?parseTime=true" up
+/goose -dir /migrations mysql "$DB_USER:$DB_PASSWORD@tcp($DB_HOST:$DB_PORT)/$DB_NAME?parseTime=true" up
 
 # シーダー
 echo "=== Running seeder ==="
-go run /app/services/hidden_waza/db/seeder/cmd/seeder
+/seeder
 
 # 本体起動
 echo "=== Starting main app ==="
